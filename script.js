d2a8f87992a6eb6c04872dff601419c29114ba68
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2022-03-16 02:36:30 +0300

    RESTRUCTURING

--- script.js
+++ script.js
@@ -6,12 +6,27 @@ console.log('Объектов всего:', numberOBJ);
 var n = 0;
 
 while (n < numberOBJ) {
-    CLUSTER.innerHTML += `
-    <div class="BOX" onclick="document.location.href = '${ARRAY[n].URL}';">
-        <div><img src="${ARRAY[n].ICON}"></div>
-        <div>${ARRAY[n].NAME}</div>
-    </div>
-    `;
+    if (ARRAY[n].NAME){ // не выводить пустые эл.
+//        var request = new XMLHttpRequest();
+//        var RESULT = request.open('GET', `${ARRAY[n].URL}favicon.ico`);
+//        console.log(`ARRAY[n].URL =`, RESULT);
+//
+//        if (RESULT == 404){
+//            CLUSTER.innerHTML += `
+//                <div class="BOX" onclick="document.location.href = '${ARRAY[n].URL}';">
+//                    <div>${ARRAY[n].NAME}</div>
+//                </div>
+//            `;
+//        }
+//        else {
+            CLUSTER.innerHTML += `
+                <div class="BOX" onclick="document.location.href = '${ARRAY[n].URL}';">
+                    <div><img src="${ARRAY[n].URL}favicon.ico"></div>
+                    <div>${ARRAY[n].NAME}</div>
+                </div>
+            `;
+//        };
+    };
     n++;
-    console.log('Кол-во завершённых итераций:', n);
 };
+console.log('Кол-во завершённых итераций:', n);
