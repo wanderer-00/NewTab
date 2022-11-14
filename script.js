4d150c53b518148aec87021cb14d871308c7e7ef
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2022-11-14 19:52:17 +0300

    1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// GOOGLE SEARCH
+document.querySelector('#run').addEventListener('click', function (e) {
+  let query = document.querySelector('#query').value;
+  
+  if(query)
+    window.location = `https://www.google.ru/search?q=${query}`;
+});
+/*-------------------------------------------------------------------*/
+
+
+
 var CLUSTER = document.getElementsByClassName('CLUSTER')[0];
 var ICON = CLUSTER.getElementsByTagName('div')[0];
 
