161b5ce0ac9b1e7a56cd0f18eb308509eeef08de
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2023-08-27 23:23:27 +0300

    -

---
+++ DATA/VIDEO.js
@@ -0,0 +1,26 @@
+var VIDEOS = [
+    {
+        "NAME":"Monster",
+        "TIME":[
+            "S": 1,
+            "E": 6,
+            "T": 5.14
+        ]
+    },
+    {
+        "NAME":"My hero academia",
+        "TIME":[
+            "S": 4,
+            "E": 15,
+            "T": 0
+        ]
+    },
+    {
+        "NAME":"",
+        "TIME":[
+            "S": 1,
+            "E": 0,
+            "T": 0
+        ]
+    },
+]
