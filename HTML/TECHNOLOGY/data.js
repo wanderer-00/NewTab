5d9174a6d5cd06dc71e36f70ee2823213b4b55df
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-03-04 07:24:33 +0300

    Добавлена страница с использованными технологиями

---
+++ HTML/TECHNOLOGY/data.js
@@ -0,0 +1,27 @@
+var array = [
+    {
+        "URL": "#",
+        "NAME": "HTML 5",
+        "ICON": "html5.svg"
+    },
+    {
+        "URL": "#",
+        "NAME": "CSS 3",
+        "ICON": "css3.svg"
+    },
+    {
+        "URL": "#",
+        "NAME": "Java Script",
+        "ICON": "js.svg"
+    },
+    {
+        "URL": "#",
+        "NAME": "JSON",
+        "ICON": "json.svg"
+    },
+    {
+        "URL": "#",
+        "NAME": "SVG",
+        "ICON": "svg.svg"
+    },
+]
---
+++ HTML/TECHNOLOGY/script.js
@@ -0,0 +1,16 @@
+var cluster = document.getElementsByClassName("cluster")[0];
+
+for (n=0; n<array.length; n++) {
+    if (array[n].NAME != "" & array[n].ICON != ""){
+        cluster.innerHTML += `
+            <a class="card" href="${array[n].URL}">
+                <div class="icon" style="background-image: url(SVG/${array[n].ICON})"></div>
+                <div class="name">${array[n].NAME}</div>
+            </a>`
+    }
+}
+
+var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart * -1;
+loadTime = loadTime / 1e12;
+loadTime = loadTime.toFixed(3);
+console.log("Время загрузки:",loadTime,"сек");
