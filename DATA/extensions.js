e202fd6b0fc4b97f154562d7de2c85e1662f4539
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-09-29 01:42:35 +0300

    -

--- DATA/extensions.js
+++ DATA/extensions.js
@@ -12,7 +12,7 @@ var array_extensions = [
         "URL": "https://chromewebstore.google.com/detail/%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B8%D1%82%D1%8C-%D0%BD%D0%BE%D0%B2%D1%83%D1%8E-%D0%B2%D0%BA%D0%BB%D0%B0%D0%B4%D0%BA%D1%83/mocklpfdimiadpbgamlgehpgpodggahe?pli=1"
     },
     {
-        "ICON": "404.svg",
+        "ICON": "tampermonkey.svg",
         "NAME": "Tampermonkey",
         "NOTE": "Расширение для запуска пользовательских скриптов на сайтах. Бесплатный проссмотр Kinopoisk, закадровый перевод видео от Яндекс и тд.",
         "URL": "https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo"
