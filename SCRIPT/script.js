895ac186f53bdff40b7480d98b427a3cc87dcf93
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-09-28 23:46:54 +0300

    Update script.js

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,19 +1,16 @@
 var cluster = document.getElementsByClassName("cluster")[0];
 
-for (n=0; n<array_all.length; n++) {
-    if (array_all[n].NAME != "" & array_all[n].ICON != ""){
-        cluster.innerHTML += `
-            <a class="card" id="card" href="${array_all[n].URL}">
-                <div class="icon" style="background-image: url(IMG/${array_all[n].ICON})"></div>
-                <div class="name">${array_all[n].NAME}</div>
-            </a>`
-    }
-}
+// for (n=0; n<array_all.length; n++) {
+//     if (array_all[n].NAME != "" & array_all[n].ICON != ""){
+//         cluster.innerHTML += `
+//             <a class="card" id="card" href="${array_all[n].URL}">
+//                 <div class="icon" style="background-image: url(IMG/${array_all[n].ICON})"></div>
+//                 <div class="name">${array_all[n].NAME}</div>
+//             </a>`
+//     }
+// }
 
-var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart * -1;
-loadTime = loadTime / 1e12;
-loadTime = loadTime.toFixed(3);
-console.log("Время загрузки:",loadTime,"сек");
+code(array_all);
 
 function code(category){
     // очистка страницы
@@ -28,6 +25,9 @@ function code(category){
                 </a>`
         }
     }
-}
+};
 
-// 111111111
+var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart * -1;
+loadTime = loadTime / 1e12;
+loadTime = loadTime.toFixed(3);
+console.log("Время загрузки:",loadTime,"сек");
