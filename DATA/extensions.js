46b7fecff2940779592e58eb53b8f59ed71b486d
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-09-28 23:42:05 +0300

    -

--- DATA/extensions.js
+++ DATA/extensions.js
@@ -1,30 +1,30 @@
 var array_extensions = [
     {
-        "ICON": "",
+        "ICON": "404.svg",
         "NAME": "AdBlock",
         "NOTE": "Блокировка рекламы на сайтах",
         "URL": "https://chromewebstore.google.com/detail/adblock-%E2%80%93-%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0-%D1%80%D0%B5%D0%BA%D0%BB/gighmmpiobklfepjocnamgkkbiglidom"
     },
     {
-        "ICON": "",
+        "ICON": "404.svg",
         "NAME": "NewTab",
         "NOTE": "Изменение новой вкладки браузера на свою",
         "URL": "https://chromewebstore.google.com/detail/%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B8%D1%82%D1%8C-%D0%BD%D0%BE%D0%B2%D1%83%D1%8E-%D0%B2%D0%BA%D0%BB%D0%B0%D0%B4%D0%BA%D1%83/mocklpfdimiadpbgamlgehpgpodggahe?pli=1"
     },
     {
-        "ICON": "",
+        "ICON": "404.svg",
         "NAME": "Tampermonkey",
         "NOTE": "Расширение для запуска пользовательских скриптов на сайтах. Бесплатный проссмотр Kinopoisk, закадровый перевод видео от Яндекс и тд.",
         "URL": "https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo"
     },
     {
-        "ICON": "",
+        "ICON": "404.svg",
         "NAME": "User JS & CSS",
         "NOTE": "Расширение для изменения сайтов и написания кастомных тем",
         "URL": "https://chromewebstore.google.com/detail/user-javascript-and-css/nbhcbdghjpllgmfilhnhkllmkecfmpld"
     },
     {
-        "ICON": "",
+        "ICON": "404.svg",
         "NAME": "Dark Reader",
         "NOTE": "Тёмная тема для всех сайтов",
         "URL": "https://chromewebstore.google.com/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh"
