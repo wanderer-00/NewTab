15aca24b1f545c72bc8430ed7c2c86ded9e9c512
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-09-29 01:51:01 +0300

    -

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,17 +1,10 @@
+// блок отрисовки вкладок
 var cluster = document.getElementsByClassName("cluster")[0];
 
-// for (n=0; n<array_all.length; n++) {
-//     if (array_all[n].NAME != "" & array_all[n].ICON != ""){
-//         cluster.innerHTML += `
-//             <a class="card" id="card" href="${array_all[n].URL}">
-//                 <div class="icon" style="background-image: url(IMG/${array_all[n].ICON})"></div>
-//                 <div class="name">${array_all[n].NAME}</div>
-//             </a>`
-//     }
-// }
-
+// загружаемая категория вкладок по умолчанию
 code(array_code);
 
+// отрисовка категорий вкладок
 function code(category){
     // очистка страницы
     cluster.innerHTML = '';
@@ -27,6 +20,7 @@ function code(category){
     }
 };
 
+// вывод времени полной отрисовки страницы
 var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart * -1;
 loadTime = loadTime / 1e12;
 loadTime = loadTime.toFixed(3);
