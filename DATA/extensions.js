686c385b8daddaacaf73784d0a39fc32fff8616e
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-10-07 12:19:33 +0300

    -

--- DATA/extensions.js
+++ DATA/extensions.js
@@ -35,4 +35,10 @@ var array_extensions = [
         "NOTE": "Востановление работы youtube.com",
         "URL": "https://chromewebstore.google.com/detail/%D1%8E%D0%B1%D1%83%D1%81%D1%82-%D1%83%D1%81%D0%BA%D0%BE%D1%80%D0%B5%D0%BD%D0%B8%D0%B5-youtube-%D1%8E/pfpolingmjapeepkjhnimfaofmlhhfbf"
     },
+    {
+        "ICON": "browsec.svg",
+        "NAME": "BS VPN",
+        "NOTE": "Бесплатный VPN для Chrome",
+        "URL": "https://chromewebstore.google.com/detail/vpn-%D0%B4%D0%BB%D1%8F-youtube-browsec-v/omghfjlpggmjjaagoclmmobgdodcjboh?hl=ru"
+    }
 ]
