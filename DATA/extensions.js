007db590435b5a805ab56c5db8538177cc4f4bce
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-10-17 16:19:24 +0300

    -

--- DATA/extensions.js
+++ DATA/extensions.js
@@ -6,7 +6,7 @@ var array_extensions = [
         "URL": "https://chromewebstore.google.com/detail/adblock-%E2%80%93-%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0-%D1%80%D0%B5%D0%BA%D0%BB/gighmmpiobklfepjocnamgkkbiglidom"
     },
     {
-        "ICON": "newtab.png",
+        "ICON": "newtab.svg",
         "NAME": "NewTab",
         "NOTE": "Изменение новой вкладки браузера на свою",
         "URL": "https://chromewebstore.google.com/detail/%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B8%D1%82%D1%8C-%D0%BD%D0%BE%D0%B2%D1%83%D1%8E-%D0%B2%D0%BA%D0%BB%D0%B0%D0%B4%D0%BA%D1%83/mocklpfdimiadpbgamlgehpgpodggahe?pli=1"
